500f3eda9ea00313e09ffc3492b4b89f1b22c494
Filip Razek <filip@razek.net>
2024-03-05 11:19:52 +0100

    Implement Java SOAP client

--- 5_soap/if4030/soap/RandomServiceClient.java
+++ 5_soap/if4030/soap/RandomServiceClient.java
@@ -8,10 +8,17 @@ public class RandomServiceClient {
             RandomServiceStub random = new RandomServiceStub( "http://" + args[0] + ":8080/axis2/services/RandomService" );
             if( args.length == 1 ) {
                 RandomServiceStub.NextRandom request = new RandomServiceStub.NextRandom();
-                // TODO: 10 appels à nextRandom
+                for (int i = 0; i < 10; i++) {
+                    int randomResult = random.nextRandom(request).get_return();
+                    System.out.printf("%d ", randomResult);
+                }
+                System.out.println("");
             }
             if( args.length == 3 ) {
-                // TODO:  appel de setBounds
+                RandomServiceStub.SetBounds request = new RandomServiceStub.SetBounds();
+                request.setMin(Integer.parseInt(args[1]));
+                request.setMax(Integer.parseInt(args[2]));
+                random.setBounds(request);
              }
         }
         catch ( Exception e ) {
