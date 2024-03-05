69e66ec2dccf70da2c59d5480460cbe189032860
Filip Razek <filip@razek.net>
2024-03-05 10:53:09 +0100

    Implement gRPC client

--- 4_grpc/random_client.cc
+++ 4_grpc/random_client.cc
@@ -1,40 +1,54 @@
 #include <iostream>
-#include <string>
 #include <memory>
+#include <string>
 
 #include <cstdlib>
 
 #include <grpc++/grpc++.h>
 
-#include "random.pb.h"
 #include "random.grpc.pb.h"
+#include "random.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 
+using if3040::grpc::NextRandomRequest;
+using if3040::grpc::NextRandomResponse;
 using if3040::grpc::RandomService;
 using if3040::grpc::SetBoundsRequest;
 using if3040::grpc::SetBoundsResponse;
-using if3040::grpc::NextRandomRequest;
-using if3040::grpc::NextRandomResponse;
 
-int main( int argc, char* argv[] )
-{
-    if( argc == 1 ) return -1;
-    std::string host{ argv[1] };
+int main(int argc, char *argv[]) {
+  if (argc == 1)
+    return -1;
+  std::string host{argv[1]};
 
-    std::shared_ptr< Channel > channel = grpc::CreateChannel( host + ":50051", grpc_impl::InsecureChannelCredentials() );
-    std::unique_ptr< RandomService::Stub > stub( RandomService::NewStub( channel ));
-    
-    if( argc == 2 ) {
-        // TODO : 10 appels à NextRandom
-    }
+  std::shared_ptr<Channel> channel = grpc::CreateChannel(
+      host + ":50051", grpc_impl::InsecureChannelCredentials());
+  std::unique_ptr<RandomService::Stub> stub(RandomService::NewStub(channel));
 
-    if( argc == 4 ) {
-        // TODO : appel à SetBounds avec argv[2] et argv[3]
-    }
+  if (argc == 2) {
+    for (int i = 0; i < 10; i++) {
+        NextRandomRequest request;
+        NextRandomResponse response;
+        ClientContext context;
+        stub->NextRandom(&context, request, &response);
+        printf("%d ", response.random());
+	}
+    printf("\n");
+  }
 
-    return 0;
+  if (argc == 4) {
+    SetBoundsRequest request;
+    SetBoundsResponse response;
+    ClientContext context;
+
+    request.set_min(atoi(argv[2]));
+    request.set_max(atoi(argv[3]));
+
+    stub->SetBounds(&context, request, &response);
+  }
+
+  return 0;
 }
-
