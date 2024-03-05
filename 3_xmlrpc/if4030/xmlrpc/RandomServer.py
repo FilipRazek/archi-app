88a451ddaf656f7fb4f5a5a9a04d87c95668e53f
Filip Razek <filip@razek.net>
2024-03-05 10:36:42 +0100

    Implement server

--- 3_xmlrpc/if4030/xmlrpc/RandomServer.py
+++ 3_xmlrpc/if4030/xmlrpc/RandomServer.py
@@ -9,8 +9,17 @@ class RequestHandler( SimpleXMLRPCRequestHandler ):
     rpc_paths = ( '/IF4030', )
 
 class RandomServer:
-    def getData(self):
-        return '42'
+    def __init__(self):
+        self.min = 0
+        self.max = 10
+    
+    def setBounds(self, min, max):
+        self.min = min
+        self.max = max
+        return 0
+    
+    def getRandom(self):
+        return int(random.random() * (self.max - self.min + 1)) + self.min
 
 # Create server
 with SimpleXMLRPCServer(( 'localhost', 8000 ),
