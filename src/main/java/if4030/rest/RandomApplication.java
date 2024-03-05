5642e7c2216c0a00c8878fe9b6914148172de45b
Filip Razek <filip@razek.net>
2024-03-05 11:21:27 +0100

    Generate Spring Application using starter

---
+++ src/main/java/if4030/rest/RandomApplication.java
@@ -0,0 +1,13 @@
+package if4030.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomApplication.class, args);
+	}
+
+}
