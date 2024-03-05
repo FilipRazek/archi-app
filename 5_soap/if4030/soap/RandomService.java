2804410f08301f87dd42eb794b9afc8c3ef3db9e
Filip Razek <filip@razek.net>
2024-03-05 11:13:02 +0100

    Implement RandomService

--- 5_soap/if4030/soap/RandomService.java
+++ 5_soap/if4030/soap/RandomService.java
@@ -1,14 +1,15 @@
 package if4030.soap;
 
 public class RandomService {
-    // TODO
+    private int min = 0;
+    private int max = 10;
         
     public void setBounds( int min, int max ) {
-        // TODO
+        this.min = min;
+        this.max = max;
     }
     
     public int nextRandom() {
-        // TODO
-        return 0;
+        return (int)Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
